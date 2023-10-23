244dd0499699720eee0408da26839e816ed972d5
swapnil chavan <chavanswapnil822@gmail.com>
2023-10-23 16:41:26 +0530

    update

---
+++ user/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Booking } from './Component/Booking.jsx';
+
+function App() {
+  return (
+    <div className="App">
+<Booking />
+    </div>
+  );
+}
+
+export default App;
